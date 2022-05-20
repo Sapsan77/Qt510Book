2fcae801d881eb899e6d2fbd6c8c40f9c96d3127
Sapsan77 <sapsan7@yandex.ru>
2022-05-20 23:41:36 +0300

    Реализация проекта на основе листингов 2.8-2.10

---
+++ chapter02/signals_and_slots/counter.h
@@ -0,0 +1,17 @@
+#ifndef COUNTER_H
+#define COUNTER_H
+
+#include <QObject>
+
+class Counter : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Counter(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // COUNTER_H
